8996f3895afac112f3aa4e3713f166d511b8a590
Faun <woecifaun@gmail.com>
2015-03-08 22:57:43 +0100

    Fix unit tests

--- src/AFUP/HaphpyBirthdayBundle/Tests/Controller/DefaultControllerTest.php
+++ src/AFUP/HaphpyBirthdayBundle/Tests/Controller/DefaultControllerTest.php
@@ -4,14 +4,40 @@ namespace AFUP\HaphpyBirthdayBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * {@inheritdoc}
+ */
 class DefaultControllerTest extends WebTestCase
 {
+    /**
+     * Check that no localized homepage request is redirected
+     */
+    public function testRedirection()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+        $response = $client->getResponse();
+
+        $this->assertTrue($response->getStatusCode() == 302);
+    }
+
+    /**
+     * Test localized index page
+     */
     public function testIndex()
     {
         $client = static::createClient();
+        $locales = [
+            'fr' => 'Bonjour',
+            'en' => 'Hi',
+        ];
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        foreach ($locales as $locale => $translation) {
+            $crawler = $client->request('GET', "/$locale/");
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+            $this->assertTrue(
+                $crawler->filter('html:contains("'.$translation.'")')->count() > 0
+            );
+        }
     }
 }
