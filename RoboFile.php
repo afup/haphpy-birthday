b010cad95049f0e1c18ed3432696cd40279c5564
Adrien Gallou <adriengallou@gmail.com>
2015-08-29 21:35:45 +0200

    assets : utilisation sass minification, et diminution du nombre de fichiers utilisés
    Le fichier css haphpy est maintenant séparé dans plusieurs fichiers sass.
    (ils ont été séparés en fonction des commentaires qu'il y avait dans le fichier css).
    
    Ce fichier et celui d'élaostrap sont maintenant concaténés et minifiés.
    Il y a donc un appel en moins pour récupérer la CSS.
    
    Les dépendances ne sont plus commités mais récupéres via bower.
    
    Au lieu d'utiliser ruby/sass/node/grunt/uglyfyjs j'ai tenté de partir sur php/scssphp/php/robo/jsqueeze.
    Cela devrais simplifier l'installation, de ne pas devoir passer par ruby/node.
    
    Les dépendances sont buildées automatiquement après le composer install.
    Pour lancer un build il faut faire un `./bin/robo build`.
    
    Lors de modifications sass il faut lancer un `./bin/robo watch`. La CSS sera buildée
    à chaque modification.
    
    Stats sur le contenu du dossier web/assets :
    
    Avant :
    ```
    JS
    --
    Nombre de fichiers
    11
    Taille des fichiers (non gzipée)
    971.377K
    Taille des fichiers (gzipée)
    303.261K
    
    CSS
    ---
    Nombre de fichiers
    4
    Taille des fichiers (non gzipée)
    477.965K
    Taille des fichiers (gzipée)
    82.225K
    
    Fonts (Woff)
    ------------
    Nombre de fichiers
    3
    Taille des fichiers (non gzipée)
    77.652K
    Taille des fichiers (gzipée)
    77.405
    ```
    
    Après :
    ```
    JS
    --
    Nombre de fichiers
    1
    Taille des fichiers (non gzipée)
    255.508K
    Taille des fichiers (gzipée)
    76.132K
    
    CSS
    ---
    Nombre de fichiers
    1
    Taille des fichiers (non gzipée)
    153.162K
    Taille des fichiers (gzipée)
    26.941K
    
    Fonts (Woff)
    ------------
    Nombre de fichiers
    3
    Taille des fichiers (non gzipée)
    87.428K
    Taille des fichiers (gzipée)
    87.131K
    ```

---
+++ RoboFile.php
@@ -0,0 +1,149 @@
+<?php
+
+class RoboFile extends \Robo\Tasks
+{
+    use Agallou\RoboHash\loadTasks;
+
+    public function watch()
+    {
+        $this->build();
+
+        $buildCss = function () {
+            $this->_cleanBase();
+            $this->_cleanCss();
+            $this->_buildCss();
+        };
+
+        $this
+            ->taskWatch()
+            ->monitor(array('app/Resources/assets/sass/main.scss'), $buildCss)
+            ->run()
+            ;
+    }
+
+    public function install()
+    {
+        $this->getDeps();
+        $this->build();
+    }
+
+    protected function getDeps()
+    {
+        $this->_cleanDir('bower_components/');
+        $this->taskBowerInstall('./bin/bowerphp')->run();
+
+        //Il n'y a pas de tag d'Elaostrap contenant les fichiers dist
+        //on clone donc le repo. Une fois qu'il y en aura un il faudra passer
+        //par le fichier bower.json
+        $this->taskGitStack()
+            ->cloneRepo('https://github.com/JeremyFagis/ElaoStrap.git', 'bower_components/ElaoStrap')
+            ->run()
+
+        ;
+
+    }
+
+    public function build()
+    {
+        $this->_clean();
+        $this->_buildCss();
+        $this->_buildJs();
+        $this->_buildOtherAssets();
+    }
+
+    protected function _buildCss()
+    {
+        $this
+            ->taskScss(['app/Resources/assets/sass/main.scss' => 'app/cache/assets/sass/main_sass.css'])
+            ->addImportPath('app/Resources/assets/sass')
+            ->run();
+
+        $this
+            ->taskConcat([
+                'bower_components/ElaoStrap/dist/css/style.css',
+                'app/cache/assets/sass/main_sass.css',
+            ])
+            ->to('app/cache/main.css')
+            ->run()
+        ;
+
+        $this
+            ->taskMinify('app/cache/main.css')
+            ->to('web/assets/css/main.css')
+            ->run()
+        ;
+
+        $this->taskHash('web/assets/css/main.css')->to('web/assets/css/')->run();
+    }
+
+    protected function _buildJs()
+    {
+        $this
+            ->taskConcat([
+                'bower_components/ElaoStrap/dist/js/main.js',
+            ])
+            ->to('app/cache/assets/main.js')
+            ->run()
+        ;
+
+        $this
+            ->taskMinify('app/cache/assets/main.js')
+            ->to('web/assets/js/main.js')
+            ->run()
+        ;
+
+        $this->taskHash('web/assets/js/main.js')->to('web/assets/js/')->run();
+    }
+
+    protected function _clean()
+    {
+        $this->_mkdir('app/cache/assets/');
+        $this->_cleanBase();
+        $this->_cleanCss();
+        $this->_cleanJs();
+        $this->_cleanOtherAssets();
+
+    }
+
+    protected function _cleanBase()
+    {
+        $this->_cleanDir('app/cache/assets/');
+    }
+
+    protected function _cleanCss()
+    {
+        $this->_mkdir('web/assets/css');
+        $this->_cleanDir('web/assets/css');
+        $this->_mkdir('app/cache/assets/sass');
+        $this->_cleanDir('app/cache/assets/sass');
+    }
+
+    protected function _cleanJs()
+    {
+        $this->_mkdir('web/assets/js');
+        $this->_cleanDir('web/assets/js');
+    }
+
+    protected function _cleanOtherAssets()
+    {
+        $this->_mkdir('web/assets/icons');
+        $this->_mkdir('web/assets/images');
+        $this->_mkdir('web/assets/videos');
+        $this->_mkdir('web/assets/fonts');
+
+        $this->_cleanDir('web/assets/icons');
+        $this->_cleanDir('web/assets/images');
+        $this->_cleanDir('web/assets/videos');
+        $this->_cleanDir('web/assets/fonts');
+    }
+
+    protected function _buildOtherAssets()
+    {
+        $this->_copyDir('app/Resources/assets/icons', 'web/assets/icons');
+        $this->_copyDir('app/Resources/assets/images', 'web/assets/images');
+        $this->_copyDir('app/Resources/assets/videos', 'web/assets/videos');
+        $this->_copyDir('bower_components/ElaoStrap/dist/fonts/elaostrap', 'web/assets/fonts/elaostrap');
+        $this->_copyDir('bower_components/fontawesome/fonts', 'web/assets/fonts/font-awesome');
+        $this->_copyDir('bower_components/dropify/dist/fonts', 'web/assets/fonts/dropify');
+    }
+}
